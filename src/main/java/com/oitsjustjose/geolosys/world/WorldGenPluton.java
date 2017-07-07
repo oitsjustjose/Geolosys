ff3aa61fb0a5ccb95735bb92612b60f7e6c0a5a8
Jose Stovall <stovallj1995@gmail.com>
2017-07-07 13:35:04 -0500

    Working world gen

--- src/main/java/com/oitsjustjose/geolosys/world/WorldGenPluton.java
+++ src/main/java/com/oitsjustjose/geolosys/world/WorldGenPluton.java
@@ -1,7 +1,7 @@
 package com.oitsjustjose.geolosys.world;
 
 import com.oitsjustjose.geolosys.Geolosys;
-import com.oitsjustjose.geolosys.util.Config;
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.block.state.pattern.BlockMatcher;
 import net.minecraft.init.Blocks;
@@ -12,49 +12,76 @@ import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 import net.minecraftforge.fml.common.IWorldGenerator;
 
+import java.util.ArrayList;
 import java.util.Random;
+
+/**
+ *
+ * A modified version of:
+ * https://github.com/BluSunrize/ImmersiveEngineering/blob/master/src/main/java/blusunrize/immersiveengineering/common/world/IEWorldGen.java
+ * Original Source & Credit: BluSunrize
+ *
+ **/
 
 public class WorldGenPluton implements IWorldGenerator
 {
-    private Config config = Geolosys.config;
-    IBlockState ore;
-    int rarity;
-    WorldGenMinable overworld;
-
-    public WorldGenPluton(IBlockState state, int rarity)
+    public static class OreGen
     {
-        this.ore = state;
-        this.rarity = rarity;
-        this.overworld = new WorldGenMinable(state, 24);
+        WorldGenMinable mineableGen;
+        int minY;
+        int maxY;
+        int chunkOccurence;
+        int weight;
+
+        public OreGen(IBlockState state, int maxVeinSize, Block replaceTarget, int minY, int maxY, int chunkOccurence, int weight)
+        {
+            this.mineableGen = new WorldGenMinable(state, maxVeinSize, BlockMatcher.forBlock(replaceTarget));
+            this.minY = minY;
+            this.maxY = maxY;
+            this.chunkOccurence = chunkOccurence;
+            this.weight = weight;
+        }
+
+        public void generate(World world, Random rand, int x, int z)
+        {
+            BlockPos pos;
+            for (int i = 0; i < chunkOccurence; i++)
+                if (rand.nextInt(100) < weight)
+                {
+                    pos = new BlockPos(x + 8, minY + rand.nextInt(maxY - minY), z + 8);
+                    mineableGen.generate(world, rand, pos);
+                }
+        }
+    }
+
+    public static ArrayList<OreGen> orespawnList = new ArrayList();
+
+    public static OreGen addOreGen(IBlockState state, int maxVeinSize, int minY, int maxY, int chunkOccurence, int weight)
+    {
+        OreGen gen = new OreGen(state, maxVeinSize, Blocks.STONE, minY, maxY, chunkOccurence, weight);
+        orespawnList.add(gen);
+        return gen;
     }
 
     @Override
-    public void generate(Random rand, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider)
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider)
     {
-        if (isDIMBlacklisted(world.provider.getDimension()))
-        {
-            return;
-        }
-        for (int i = 0; i < rarity; i++)
-        {
-            int x = chunkX * 16 + rand.nextInt(16);
-            int y = rand.nextInt(70);
-            int z = chunkZ * 16 + rand.nextInt(16);
-
-            overworld.generate(world, rand, new BlockPos(x, y, z));
-        }
+        this.generateOres(random, chunkX, chunkZ, world);
     }
 
-
-    private boolean isDIMBlacklisted(int dim)
+    public void generateOres(Random random, int chunkX, int chunkZ, World world)
     {
-        for (int d : config.blacklistedDIMs)
-        {
+        if (!isDIMBlacklisted(world.provider.getDimension()))
+            for (OreGen gen : orespawnList)
+                gen.generate(world, random, (chunkX * 16), (chunkZ * 16));
+    }
+
+    public boolean isDIMBlacklisted(int dim)
+    {
+        for (int d : Geolosys.config.blacklistedDIMs)
             if (d == dim)
-            {
                 return true;
-            }
-        }
         return false;
     }
+
 }
