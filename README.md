81222f41ba487fb3574e523387a4cd05e0b76192
Jose Stovall <stovallj1995@gmail.com>
2017-07-07 15:51:39 -0500

    Configs for default world generation overrides

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A **geolo**gical **sys**tem in Minecraft
 ### To-Do:
 - [ ] Dynamic Ingots
 - [ ] Smelting Recipes for clusters
-- [ ] Configs for default world generation overrides
+- [x] Configs for default world generation overrides
 - [ ] Check facts about ores added, to be certain
 - [ ] Drop sulfur (if exists) alongside sphalerite?
 - [ ] Modify / Play with Galena drops
--- src/main/java/com/oitsjustjose/geolosys/world/WorldGenOverride.java
+++ src/main/java/com/oitsjustjose/geolosys/world/WorldGenOverride.java
@@ -15,12 +15,19 @@ public class WorldGenOverride
     public void onOrePopulate(OreGenEvent.GenerateMinable event)
     {
         if (Geolosys.config.disableIron && event.getType() == OreGenEvent.GenerateMinable.EventType.IRON)
-        {
             event.setResult(Event.Result.DENY);
-        }
-        if (event.getType() == OreGenEvent.GenerateMinable.EventType.COAL || event.getType() == OreGenEvent.GenerateMinable.EventType.DIAMOND || event.getType() == OreGenEvent.GenerateMinable.EventType.GOLD || event.getType() == OreGenEvent.GenerateMinable.EventType.REDSTONE || event.getType() == OreGenEvent.GenerateMinable.EventType.LAPIS || event.getType() == OreGenEvent.GenerateMinable.EventType.ANDESITE || event.getType() == OreGenEvent.GenerateMinable.EventType.GRANITE || event.getType() == OreGenEvent.GenerateMinable.EventType.DIORITE)
-        {
+        if (Geolosys.config.modGold && event.getType() == OreGenEvent.GenerateMinable.EventType.GOLD)
             event.setResult(Event.Result.DENY);
-        }
+        if (Geolosys.config.modDiamond && event.getType() == OreGenEvent.GenerateMinable.EventType.DIAMOND)
+            event.setResult(Event.Result.DENY);
+        if (Geolosys.config.modCoal && event.getType() == OreGenEvent.GenerateMinable.EventType.COAL)
+            event.setResult(Event.Result.DENY);
+        if (Geolosys.config.modRedstone && event.getType() == OreGenEvent.GenerateMinable.EventType.REDSTONE)
+            event.setResult(Event.Result.DENY);
+        if (Geolosys.config.modLapis && event.getType() == OreGenEvent.GenerateMinable.EventType.LAPIS)
+            event.setResult(Event.Result.DENY);
+        if (Geolosys.config.modStones)
+            if (event.getType() == OreGenEvent.GenerateMinable.EventType.ANDESITE || event.getType() == OreGenEvent.GenerateMinable.EventType.GRANITE || event.getType() == OreGenEvent.GenerateMinable.EventType.DIORITE)
+                event.setResult(Event.Result.DENY);
     }
 }
