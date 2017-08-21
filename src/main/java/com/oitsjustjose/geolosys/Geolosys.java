ba79a9b43cd1ce0a0902b581b888e9e6feba1f68
oitsjustjose <stovallj1995@gmail.com>
2017-08-21 11:38:02 -0400

    Adjust Quartz again - realistically it is formed at high temp + pressure

--- src/main/java/com/oitsjustjose/geolosys/Geolosys.java
+++ src/main/java/com/oitsjustjose/geolosys/Geolosys.java
@@ -129,7 +129,7 @@ public class Geolosys
         if (config.modLapis)
             OreGenerator.addOreGen(ORE_VANILLA.getStateFromMeta(3), config.clusterSizeLapis, 10, 24, config.chanceLapis);
         if (config.modQuartz)
-            OreGenerator.addOreGen(ORE_VANILLA.getStateFromMeta(4), config.clusterSizeQuartz, 48, 56, config.chanceQuartz);
+            OreGenerator.addOreGen(ORE_VANILLA.getStateFromMeta(4), config.clusterSizeQuartz, 6, 29, config.chanceQuartz);
         if (config.modDiamond)
             OreGenerator.addOreGen(ORE_VANILLA.getStateFromMeta(5), config.clusterSizeKimberlite, 2, 15, config.chanceKimberlite);
 
