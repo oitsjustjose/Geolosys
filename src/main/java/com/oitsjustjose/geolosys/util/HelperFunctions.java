682e481f97136bc139c3b0f8fe7938a3407f58ed
oitsjustjose <stovallj1995@gmail.com>
2017-10-16 22:34:36 -0400

    I'll write my own I18n with blackjack and hookers!

--- src/main/java/com/oitsjustjose/geolosys/util/HelperFunctions.java
+++ src/main/java/com/oitsjustjose/geolosys/util/HelperFunctions.java
@@ -1,8 +1,16 @@
 package com.oitsjustjose.geolosys.util;
 
+import com.oitsjustjose.geolosys.Geolosys;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
 import net.minecraft.util.EnumFacing;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class HelperFunctions
 {
@@ -18,4 +26,29 @@ public class HelperFunctions
         }
     }
 
+    @SideOnly(Side.CLIENT)
+    public static String getTranslation(String toTranslate)
+    {
+        String langFile = Minecraft.getMinecraft().gameSettings.language;
+        langFile = langFile.substring(0, langFile.indexOf("_")) + langFile.substring(langFile.indexOf("_")).toUpperCase();
+        InputStream in = Geolosys.class.getResourceAsStream("/assets/geolosys/lang/" + langFile + ".lang");
+        try
+        {
+            for (String s : IOUtils.readLines(in, "utf-8"))
+            {
+                if (s.indexOf("=") == -1)
+                {
+                    continue;
+                }
+                if (s.contains(toTranslate))
+                {
+                    return s.substring(s.indexOf("=") + 1);
+                }
+            }
+        }
+        catch (IOException e)
+        {
+        }
+        return toTranslate;
+    }
 }
