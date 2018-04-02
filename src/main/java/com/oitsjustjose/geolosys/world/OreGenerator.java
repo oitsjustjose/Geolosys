67d776ba98d6c6ebea64889f5d409d4321de70aa
oitsjustjose <stovallj1995@gmail.com>
2018-04-01 20:52:19 -0400

    Remove another debug >_>

--- src/main/java/com/oitsjustjose/geolosys/world/OreGenerator.java
+++ src/main/java/com/oitsjustjose/geolosys/world/OreGenerator.java
@@ -88,7 +88,6 @@ public class OreGenerator implements IWorldGenerator
                 }
                 pluton.generate(world, rand, new BlockPos(x + 8, y, z + 8));
                 GeolosysAPI.putWorldDeposit(new ChunkPos(x / 16, z / 16), state.getBlock().getRegistryName() + ":" + state.getBlock().getMetaFromState(state));
-                System.out.println(state.getBlock().getRegistryName() + ":" + state.getBlock().getMetaFromState(state));
                 GeolosysAPI.writeToFile();
                 Geolosys.getInstance().chunkOreGen.addChunk(new ChunkPos(x / 16, z / 16), world, GeolosysAPI.oreBlocks.get(state));
             }
