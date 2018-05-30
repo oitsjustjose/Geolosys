a68ac4b312014b8491e5a663793a250dd08fe292
oitsjustjose <stovallj1995@gmail.com>
2018-05-30 16:50:44 -0400

    Expand Propick Abilities! (Closes #122)

--- src/main/java/com/oitsjustjose/geolosys/items/ItemProPick.java
+++ src/main/java/com/oitsjustjose/geolosys/items/ItemProPick.java
@@ -4,9 +4,13 @@ import com.oitsjustjose.geolosys.Geolosys;
 import com.oitsjustjose.geolosys.api.GeolosysAPI;
 import com.oitsjustjose.geolosys.config.ModConfig;
 import com.oitsjustjose.geolosys.util.HelperFunctions;
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.debug.DebugRendererChunkBorder;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -17,6 +21,7 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -27,8 +32,11 @@ import java.util.List;
 
 public class ItemProPick extends Item
 {
+    private boolean showingChunkBorders;
+
     public ItemProPick()
     {
+        this.showingChunkBorders = false;
         this.setMaxStackSize(1);
         this.setCreativeTab(CreativeTabs.TOOLS);
         this.setRegistryName(new ResourceLocation(Geolosys.MODID, "PRO_PICK"));
@@ -63,6 +71,20 @@ public class ItemProPick extends Item
     }
 
     @Override
+    @SideOnly(Side.CLIENT)
+    public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected)
+    {
+        if (isSelected && !showingChunkBorders)
+        {
+            showingChunkBorders = Minecraft.getMinecraft().debugRenderer.toggleChunkBorders();
+        }
+        else if (!isSelected && showingChunkBorders)
+        {
+            showingChunkBorders = Minecraft.getMinecraft().debugRenderer.toggleChunkBorders();
+        }
+    }
+
+    @Override
     public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
     {
         if (worldIn.isRemote)
@@ -71,81 +93,109 @@ public class ItemProPick extends Item
             return EnumActionResult.PASS;
         }
 
-        int xStart;
-        int xEnd;
-        int yStart;
-        int yEnd;
-        int zStart;
-        int zEnd;
-        int confAmt = ModConfig.featureControl.proPickRange;
-        int confDmt = ModConfig.featureControl.proPickDiameter;
-
-        boolean found = false;
-
-        switch (facing)
+        if (pos.getY() >= worldIn.getTopSolidOrLiquidBlock(pos).getY() - 1)
         {
-            case UP:
-                xStart = -(confDmt / 2);
-                xEnd = confDmt / 2;
-                yStart = -confAmt;
-                yEnd = 0;
-                zStart = -(confDmt / 2);
-                zEnd = (confDmt / 2);
-                found = isFound(player, worldIn, pos, facing, xStart, xEnd, yStart, yEnd, zStart, zEnd);
-                break;
-            case DOWN:
-                xStart = -(confDmt / 2);
-                xEnd = confDmt / 2;
-                yStart = 0;
-                yEnd = confAmt;
-                zStart = -(confDmt / 2);
-                zEnd = confDmt / 2;
-                found = isFound(player, worldIn, pos, facing, xStart, xEnd, yStart, yEnd, zStart, zEnd);
-                break;
-            case NORTH:
-                xStart = -(confDmt / 2);
-                xEnd = confDmt / 2;
-                yStart = -(confDmt / 2);
-                yEnd = confDmt / 2;
-                zStart = 0;
-                zEnd = confAmt;
-                found = isFound(player, worldIn, pos, facing, xStart, xEnd, yStart, yEnd, zStart, zEnd);
-                break;
-            case SOUTH:
-                xStart = -(confDmt / 2);
-                xEnd = confDmt / 2;
-                yStart = -(confDmt / 2);
-                yEnd = confDmt / 2;
-                zStart = -confAmt;
-                zEnd = 0;
-
-                found = isFound(player, worldIn, pos, facing, xStart, xEnd, yStart, yEnd, zStart, zEnd);
-                break;
-            case EAST:
-                xStart = -(confAmt);
-                xEnd = 0;
-                yStart = -(confDmt / 2);
-                yEnd = confDmt / 2;
-                zStart = -(confDmt / 2);
-                zEnd = confDmt / 2;
-
-                found = isFound(player, worldIn, pos, facing, xStart, xEnd, yStart, yEnd, zStart, zEnd);
-                break;
-            case WEST:
-                xStart = 0;
-                xEnd = confAmt;
-                yStart = -(confDmt / 2);
-                yEnd = confDmt / 2;
-                zStart = -(confDmt / 2);
-                zEnd = confDmt / 2;
-                found = isFound(player, worldIn, pos, facing, xStart, xEnd, yStart, yEnd, zStart, zEnd);
-                break;
+            String depositInChunk = HelperFunctions.getTranslation("geolosys.pro_pick.tooltip.nonefound");
+            for (GeolosysAPI.ChunkPosSerializable chunkPos : GeolosysAPI.getCurrentWorldDeposits().keySet())
+            {
+                if (chunkPos.getX() == pos.getX() / 16)
+                {
+                    if (chunkPos.getZ() == pos.getZ() / 16)
+                    {
+                        if (chunkPos.getDimension() == worldIn.provider.getDimension())
+                        {
+                            String rawName = GeolosysAPI.getCurrentWorldDeposits().get(chunkPos);
+                            try
+                            {
+                                depositInChunk = new ItemStack(ForgeRegistries.BLOCKS.getValue(new ResourceLocation(rawName.split(":")[0], rawName.split(":")[1])), 1, Integer.parseInt(rawName.split(":")[2])).getDisplayName() + HelperFunctions.getTranslation("geolosys.pro_pick.tooltip.found");
+                            }
+                            catch (NullPointerException ignored)
+                            {
+                                System.out.println("Fuck");
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+            player.sendStatusMessage(new TextComponentString(depositInChunk), true);
         }
-
-
-        if (!found)
+        else
         {
-            player.sendStatusMessage(new TextComponentString("No deposits found"), true);
+            int xStart;
+            int xEnd;
+            int yStart;
+            int yEnd;
+            int zStart;
+            int zEnd;
+            int confAmt = ModConfig.featureControl.proPickRange;
+            int confDmt = ModConfig.featureControl.proPickDiameter;
+
+            boolean found = false;
+
+            switch (facing)
+            {
+                case UP:
+                    xStart = -(confDmt / 2);
+                    xEnd = confDmt / 2;
+                    yStart = -confAmt;
+                    yEnd = 0;
+                    zStart = -(confDmt / 2);
+                    zEnd = (confDmt / 2);
+                    found = isFound(player, worldIn, pos, facing, xStart, xEnd, yStart, yEnd, zStart, zEnd);
+                    break;
+                case DOWN:
+                    xStart = -(confDmt / 2);
+                    xEnd = confDmt / 2;
+                    yStart = 0;
+                    yEnd = confAmt;
+                    zStart = -(confDmt / 2);
+                    zEnd = confDmt / 2;
+                    found = isFound(player, worldIn, pos, facing, xStart, xEnd, yStart, yEnd, zStart, zEnd);
+                    break;
+                case NORTH:
+                    xStart = -(confDmt / 2);
+                    xEnd = confDmt / 2;
+                    yStart = -(confDmt / 2);
+                    yEnd = confDmt / 2;
+                    zStart = 0;
+                    zEnd = confAmt;
+                    found = isFound(player, worldIn, pos, facing, xStart, xEnd, yStart, yEnd, zStart, zEnd);
+                    break;
+                case SOUTH:
+                    xStart = -(confDmt / 2);
+                    xEnd = confDmt / 2;
+                    yStart = -(confDmt / 2);
+                    yEnd = confDmt / 2;
+                    zStart = -confAmt;
+                    zEnd = 0;
+
+                    found = isFound(player, worldIn, pos, facing, xStart, xEnd, yStart, yEnd, zStart, zEnd);
+                    break;
+                case EAST:
+                    xStart = -(confAmt);
+                    xEnd = 0;
+                    yStart = -(confDmt / 2);
+                    yEnd = confDmt / 2;
+                    zStart = -(confDmt / 2);
+                    zEnd = confDmt / 2;
+
+                    found = isFound(player, worldIn, pos, facing, xStart, xEnd, yStart, yEnd, zStart, zEnd);
+                    break;
+                case WEST:
+                    xStart = 0;
+                    xEnd = confAmt;
+                    yStart = -(confDmt / 2);
+                    yEnd = confDmt / 2;
+                    zStart = -(confDmt / 2);
+                    zEnd = confDmt / 2;
+                    found = isFound(player, worldIn, pos, facing, xStart, xEnd, yStart, yEnd, zStart, zEnd);
+                    break;
+            }
+            if (!found)
+            {
+                player.sendStatusMessage(new TextComponentString("No deposits found"), true);
+            }
         }
         player.swingArm(hand);
         return EnumActionResult.SUCCESS;
