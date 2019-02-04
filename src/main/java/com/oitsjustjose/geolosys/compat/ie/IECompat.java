adf93e2c432e3e53239ca208248e978413fe263e
oitsjustjose <stovallj1995@gmail.com>
2019-02-04 10:11:18 -0500

    Add native IE Crusher compat with Geolosys raw ore blocks (Closes #117)

--- src/main/java/com/oitsjustjose/geolosys/compat/ie/IECompat.java
+++ src/main/java/com/oitsjustjose/geolosys/compat/ie/IECompat.java
@@ -4,9 +4,11 @@ import java.util.LinkedHashMap;
 
 import com.oitsjustjose.geolosys.Geolosys;
 import com.oitsjustjose.geolosys.common.config.ConfigOres;
+import com.oitsjustjose.geolosys.common.config.ModConfig;
 import com.oitsjustjose.geolosys.compat.ModMaterials;
 
 import blusunrize.immersiveengineering.api.tool.ExcavatorHandler;
+import blusunrize.immersiveengineering.api.crafting.CrusherRecipe;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
@@ -113,11 +115,20 @@ public class IECompat
 
         // Add custom Geolosys entries
         ConfigOres oreConf = Geolosys.getInstance().configOres;
+        CrusherRecipe crusherRecipe;
         if (oreConf.coal.getChance() > 0)
         {
             ExcavatorHandler.addMineral("Coal", Geolosys.getInstance().configOres.coal.getChance(), 0.1F, new String[]
             { "oreBlockCoal" }, new float[]
             { 1.0F });
+
+            crusherRecipe = new CrusherRecipe(new ItemStack(Items.COAL, 1, 0),
+                    new ItemStack(Geolosys.getInstance().ORE_VANILLA, 1, 0), 1000);
+            if (OreDictionary.doesOreNameExist("dustSulfur") && OreDictionary.getOres("dustSulfur").size() > 0)
+            {
+                crusherRecipe = crusherRecipe.addToSecondaryOutput(OreDictionary.getOres("dustSulfur").get(0), .02F);
+            }
+            CrusherRecipe.recipeList.add(crusherRecipe);
         }
         if (oreConf.cinnabar.getChance() > 0)
         {
@@ -125,12 +136,36 @@ public class IECompat
                     new String[]
                     { "oreBlockCinnabar" }, new float[]
                     { 1.0F });
+
+            crusherRecipe = new CrusherRecipe(new ItemStack(Items.REDSTONE, 1, 0),
+                    new ItemStack(Geolosys.getInstance().ORE_VANILLA, 1, 1), 1000);
+
+            if (ModMaterials.EXU_MATERIAL != null)
+            {
+                crusherRecipe = crusherRecipe.addToSecondaryOutput(new ItemStack(ModMaterials.EXU_MATERIAL), .033F);
+            }
+            if (ModMaterials.TE_MATERIAL != null)
+            {
+                crusherRecipe = crusherRecipe.addToSecondaryOutput(new ItemStack(ModMaterials.TE_MATERIAL, 1, 866),
+                        .0166F);
+            }
+            if (ModMaterials.NC_GEM != null)
+            {
+                crusherRecipe = crusherRecipe.addToSecondaryOutput(new ItemStack(ModMaterials.NC_GEM, 1, 0), 0.25F);
+            }
+
+            CrusherRecipe.recipeList.add(crusherRecipe);
         }
         if (oreConf.gold.getChance() > 0)
         {
             ExcavatorHandler.addMineral("Gold", Geolosys.getInstance().configOres.gold.getChance(), 0.05F, new String[]
             { "oreBlockGold" }, new float[]
             { 1.0F });
+
+            crusherRecipe = new CrusherRecipe(new ItemStack(Geolosys.getInstance().CLUSTER, 1, 1),
+                    new ItemStack(Geolosys.getInstance().ORE_VANILLA, 1, 2), 1000);
+
+            CrusherRecipe.recipeList.add(crusherRecipe);
         }
         if (oreConf.lapis.getChance() > 0)
         {
@@ -138,6 +173,14 @@ public class IECompat
                     new String[]
                     { "oreBlockLapis" }, new float[]
                     { 1.0F });
+
+            crusherRecipe = new CrusherRecipe(new ItemStack(Items.DYE, 10, 4),
+                    new ItemStack(Geolosys.getInstance().ORE_VANILLA, 1, 3), 1000);
+            if (ModMaterials.NC_GEM != null)
+            {
+                crusherRecipe = crusherRecipe.addToSecondaryOutput(new ItemStack(ModMaterials.NC_GEM, 1, 2), .95F);
+            }
+            CrusherRecipe.recipeList.add(crusherRecipe);
         }
         if (oreConf.quartz.getChance() > 0)
         {
@@ -145,6 +188,25 @@ public class IECompat
                     new String[]
                     { "oreBlockQuartz" }, new float[]
                     { 1.0F });
+
+            crusherRecipe = new CrusherRecipe(new ItemStack(Items.QUARTZ, 9, 0),
+                    new ItemStack(Geolosys.getInstance().ORE_VANILLA, 1, 4), 1000);
+
+            if (ModMaterials.AE_MATERIAL != null)
+            {
+                crusherRecipe = crusherRecipe.addToSecondaryOutput(new ItemStack(ModMaterials.AE_MATERIAL, 1, 0), .2F);
+                crusherRecipe = crusherRecipe.addToSecondaryOutput(new ItemStack(ModMaterials.AE_MATERIAL, 1, 1), .08F);
+            }
+            if (ModMaterials.BLACK_QUARTZ != null)
+            {
+                crusherRecipe = crusherRecipe.addToSecondaryOutput(new ItemStack(ModMaterials.BLACK_QUARTZ, 1, 5),
+                        .16F);
+            }
+            if (ModMaterials.NC_GEM != null)
+            {
+                crusherRecipe = crusherRecipe.addToSecondaryOutput(new ItemStack(ModMaterials.NC_DUST, 1, 10), .18F);
+            }
+            CrusherRecipe.recipeList.add(crusherRecipe);
         }
         if (oreConf.kimberlite.getChance() > 0)
         {
@@ -152,6 +214,10 @@ public class IECompat
                     new String[]
                     { "oreBlockKimberlite" }, new float[]
                     { 1.0F });
+
+            crusherRecipe = new CrusherRecipe(new ItemStack(Items.DIAMOND, 1, 0),
+                    new ItemStack(Geolosys.getInstance().ORE_VANILLA, 1, 5), 1000);
+            CrusherRecipe.recipeList.add(crusherRecipe);
         }
         if (oreConf.beryl.getChance() > 0)
         {
@@ -159,6 +225,10 @@ public class IECompat
                     new String[]
                     { "oreBlockBeryl" }, new float[]
                     { 1.0F });
+
+            crusherRecipe = new CrusherRecipe(new ItemStack(Items.EMERALD, 1, 0),
+                    new ItemStack(Geolosys.getInstance().ORE_VANILLA, 1, 6), 1000);
+            CrusherRecipe.recipeList.add(crusherRecipe);
         }
         if (oreConf.hematite.getChance() > 0)
         {
@@ -166,6 +236,10 @@ public class IECompat
                     new String[]
                     { "oreBlockHematite" }, new float[]
                     { 1.0F });
+
+            crusherRecipe = new CrusherRecipe(new ItemStack(Geolosys.getInstance().CLUSTER, 1, 0),
+                    new ItemStack(Geolosys.getInstance().ORE, 1, 0), 1000);
+            CrusherRecipe.recipeList.add(crusherRecipe);
         }
         if (oreConf.limonite.getChance() > 0)
         {
@@ -173,6 +247,14 @@ public class IECompat
                     new String[]
                     { "oreBlockLimonite" }, new float[]
                     { 1.0F });
+
+            crusherRecipe = new CrusherRecipe(new ItemStack(Geolosys.getInstance().CLUSTER, 1, 0),
+                    new ItemStack(Geolosys.getInstance().ORE, 1, 1), 1000);
+            crusherRecipe = crusherRecipe.addToSecondaryOutput(new ItemStack(Geolosys.getInstance().CLUSTER, 1, 7),
+                    .2F);
+
+            CrusherRecipe.recipeList.add(crusherRecipe);
+
         }
         if (oreConf.malachite.getChance() > 0)
         {
@@ -180,6 +262,11 @@ public class IECompat
                     new String[]
                     { "oreBlockMalachite" }, new float[]
                     { 1.0F });
+
+            crusherRecipe = new CrusherRecipe(new ItemStack(Geolosys.getInstance().CLUSTER, 1, 2),
+                    new ItemStack(Geolosys.getInstance().ORE, 1, 2), 1000);
+
+            CrusherRecipe.recipeList.add(crusherRecipe);
         }
         if (oreConf.azurite.getChance() > 0)
         {
@@ -187,6 +274,13 @@ public class IECompat
                     new String[]
                     { "oreBlockAzurite" }, new float[]
                     { 1.0F });
+
+            crusherRecipe = new CrusherRecipe(new ItemStack(Geolosys.getInstance().CLUSTER, 1, 2),
+                    new ItemStack(Geolosys.getInstance().ORE, 1, 3), 1000);
+            crusherRecipe = crusherRecipe.addToSecondaryOutput(new ItemStack(Geolosys.getInstance().CLUSTER, 1, 2),
+                    .4F);
+
+            CrusherRecipe.recipeList.add(crusherRecipe);
         }
         if (oreConf.cassiterite.getChance() > 0)
         {
@@ -194,6 +288,11 @@ public class IECompat
                     new String[]
                     { "oreBlockCassiterite" }, new float[]
                     { 1.0F });
+
+            crusherRecipe = new CrusherRecipe(new ItemStack(Geolosys.getInstance().CLUSTER, 1, 3),
+                    new ItemStack(Geolosys.getInstance().ORE, 1, 4), 1000);
+
+            CrusherRecipe.recipeList.add(crusherRecipe);
         }
         if (oreConf.teallite.getChance() > 0)
         {
@@ -201,6 +300,13 @@ public class IECompat
                     new String[]
                     { "oreBlockTeallite" }, new float[]
                     { 1.0F });
+
+            crusherRecipe = new CrusherRecipe(new ItemStack(Geolosys.getInstance().CLUSTER, 1, 3),
+                    new ItemStack(Geolosys.getInstance().ORE, 1, 5), 1000);
+            crusherRecipe = crusherRecipe.addToSecondaryOutput(new ItemStack(Geolosys.getInstance().CLUSTER, 1, 3),
+                    .4F);
+
+            CrusherRecipe.recipeList.add(crusherRecipe);
         }
         if (oreConf.galena.getChance() > 0)
         {
@@ -208,6 +314,12 @@ public class IECompat
                     new String[]
                     { "oreBlockGalena" }, new float[]
                     { 1.0F });
+
+            crusherRecipe = new CrusherRecipe(new ItemStack(Geolosys.getInstance().CLUSTER, 1, 4),
+                    new ItemStack(Geolosys.getInstance().ORE, 1, 6), 1000);
+            crusherRecipe = crusherRecipe.addToSecondaryOutput(new ItemStack(Geolosys.getInstance().CLUSTER, 1, 5), 1F);
+
+            CrusherRecipe.recipeList.add(crusherRecipe);
         }
         if (oreConf.bauxite.getChance() > 0)
         {
@@ -215,6 +327,11 @@ public class IECompat
                     new String[]
                     { "oreBlockBauxite" }, new float[]
                     { 1.0F });
+
+            crusherRecipe = new CrusherRecipe(new ItemStack(Geolosys.getInstance().CLUSTER, 1, 6),
+                    new ItemStack(Geolosys.getInstance().ORE, 1, 7), 1000);
+
+            CrusherRecipe.recipeList.add(crusherRecipe);
         }
         if (oreConf.platinum.getChance() > 0)
         {
@@ -222,6 +339,26 @@ public class IECompat
                     new String[]
                     { "oreBlockPlatinum" }, new float[]
                     { 1.0F });
+
+            if (ModConfig.featureControl.enableOsmiumExclusively)
+            {
+                crusherRecipe = new CrusherRecipe(new ItemStack(Geolosys.getInstance().CLUSTER, 1, 12),
+                        new ItemStack(Geolosys.getInstance().ORE, 1, 8), 1000);
+            }
+            else if (ModConfig.featureControl.enableOsmium)
+            {
+                crusherRecipe = new CrusherRecipe(new ItemStack(Geolosys.getInstance().CLUSTER, 1, 8),
+                        new ItemStack(Geolosys.getInstance().ORE, 1, 8), 1000);
+                crusherRecipe = crusherRecipe.addToSecondaryOutput(new ItemStack(Geolosys.getInstance().CLUSTER, 1, 12),
+                        .5F);
+            }
+            else
+            {
+                crusherRecipe = new CrusherRecipe(new ItemStack(Geolosys.getInstance().CLUSTER, 1, 8),
+                        new ItemStack(Geolosys.getInstance().ORE, 1, 8), 1000);
+            }
+
+            CrusherRecipe.recipeList.add(crusherRecipe);
         }
         if (oreConf.autunite.getChance() > 0)
         {
@@ -229,6 +366,20 @@ public class IECompat
                     new String[]
                     { "oreBlockAutunite" }, new float[]
                     { 1.0F });
+
+            if (ModConfig.featureControl.enableYellorium)
+            {
+                crusherRecipe = new CrusherRecipe(new ItemStack(Geolosys.getInstance().CLUSTER, 1, 9),
+                        new ItemStack(Geolosys.getInstance().ORE, 1, 9), 1000);
+                crusherRecipe = crusherRecipe.addToSecondaryOutput(new ItemStack(Geolosys.getInstance().CLUSTER, 1, 11),
+                        .5F);
+            }
+            else
+            {
+                crusherRecipe = new CrusherRecipe(new ItemStack(Geolosys.getInstance().CLUSTER, 1, 9),
+                        new ItemStack(Geolosys.getInstance().ORE, 1, 9), 1000);
+            }
+            CrusherRecipe.recipeList.add(crusherRecipe);
         }
         if (oreConf.sphalerite.getChance() > 0)
         {
@@ -236,6 +387,11 @@ public class IECompat
                     new String[]
                     { "oreBlockSphalerite" }, new float[]
                     { 1.0F });
+
+            crusherRecipe = new CrusherRecipe(new ItemStack(Geolosys.getInstance().CLUSTER, 1, 10),
+                    new ItemStack(Geolosys.getInstance().ORE, 1, 10), 1000);
+            CrusherRecipe.recipeList.add(crusherRecipe);
+
         }
     }
 }
