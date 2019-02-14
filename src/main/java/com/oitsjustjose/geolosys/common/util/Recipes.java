7b0e338db03833dd2a66ef6ed451a78691df6030
oitsjustjose <stovallj1995@gmail.com>
2019-02-13 20:13:27 -0500

    Don't req. ore to be generating to enable smelting (closes #178)

--- src/main/java/com/oitsjustjose/geolosys/common/util/Recipes.java
+++ src/main/java/com/oitsjustjose/geolosys/common/util/Recipes.java
@@ -16,51 +16,18 @@ public class Recipes
     {
         boolean bwm = Loader.isModLoaded("betterwithmods");
 
-        if (configOres.hematite.getChance() > 0 || configOres.limonite.getChance() > 0)
-        {
-            smeltSafely(new ItemStack(CLUSTER, 1, 0), bwm ? "nuggetIron" : "ingotIron");
-        }
-        if (configOres.gold.getChance() > 0)
-        {
-            smeltSafely(new ItemStack(CLUSTER, 1, 1), bwm ? "nuggetGold" : "ingotGold");
-        }
-        if (configOres.malachite.getChance() > 0 || configOres.azurite.getChance() > 0)
-        {
-            smeltSafely(new ItemStack(CLUSTER, 1, 2), bwm ? "nuggetCopper" : "ingotCopper");
-        }
-        if (configOres.cassiterite.getChance() > 0 || configOres.teallite.getChance() > 0)
-        {
-            smeltSafely(new ItemStack(CLUSTER, 1, 3), bwm ? "nuggetTin" : "ingotTin");
-        }
-        if (configOres.galena.getChance() > 0)
-        {
-            smeltSafely(new ItemStack(CLUSTER, 1, 4), bwm ? "nuggetSilver" : "ingotSilver");
-            smeltSafely(new ItemStack(CLUSTER, 1, 5), bwm ? "nuggetLead" : "ingotLead");
-        }
-        if (configOres.bauxite.getChance() > 0)
-        {
-            smeltSafely(new ItemStack(CLUSTER, 1, 6), bwm ? "nuggetAluminum" : "ingotAluminum");
-        }
-        if (configOres.limonite.getChance() > 0)
-        {
-            smeltSafely(new ItemStack(CLUSTER, 1, 7), bwm ? "nuggetNickel" : "ingotNickel");
-        }
-        if (configOres.platinum.getChance() > 0)
-        {
-            smeltSafely(new ItemStack(CLUSTER, 1, 8), bwm ? "nuggetPlatinum" : "ingotPlatinum");
-        }
-        if (configOres.sphalerite.getChance() > 0)
-        {
-            smeltSafely(new ItemStack(CLUSTER, 1, 10), bwm ? "nuggetZinc" : "ingotZinc");
-        }
-        if (ModConfig.featureControl.enableYellorium)
-        {
-            smeltSafely(new ItemStack(CLUSTER, 1, 11), bwm ? "nuggetYellorium" : "ingotYellorium");
-        }
-        if (ModConfig.featureControl.enableOsmium)
-        {
-            smeltSafely(new ItemStack(CLUSTER, 1, 12), bwm ? "nuggetOsmium" : "ingotOsmium");
-        }
+        smeltSafely(new ItemStack(CLUSTER, 1, 0), bwm ? "nuggetIron" : "ingotIron");
+        smeltSafely(new ItemStack(CLUSTER, 1, 1), bwm ? "nuggetGold" : "ingotGold");
+        smeltSafely(new ItemStack(CLUSTER, 1, 2), bwm ? "nuggetCopper" : "ingotCopper");
+        smeltSafely(new ItemStack(CLUSTER, 1, 3), bwm ? "nuggetTin" : "ingotTin");
+        smeltSafely(new ItemStack(CLUSTER, 1, 4), bwm ? "nuggetSilver" : "ingotSilver");
+        smeltSafely(new ItemStack(CLUSTER, 1, 5), bwm ? "nuggetLead" : "ingotLead");
+        smeltSafely(new ItemStack(CLUSTER, 1, 6), bwm ? "nuggetAluminum" : "ingotAluminum");
+        smeltSafely(new ItemStack(CLUSTER, 1, 7), bwm ? "nuggetNickel" : "ingotNickel");
+        smeltSafely(new ItemStack(CLUSTER, 1, 8), bwm ? "nuggetPlatinum" : "ingotPlatinum");
+        smeltSafely(new ItemStack(CLUSTER, 1, 10), bwm ? "nuggetZinc" : "ingotZinc");
+        smeltSafely(new ItemStack(CLUSTER, 1, 11), bwm ? "nuggetYellorium" : "ingotYellorium");
+        smeltSafely(new ItemStack(CLUSTER, 1, 12), bwm ? "nuggetOsmium" : "ingotOsmium");
     }
 
     private static void smeltSafely(ItemStack input, String oreDictName)
