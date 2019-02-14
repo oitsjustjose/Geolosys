637e8fdd4d53f81ceca958e68f8ee4ccfdcff05d
oitsjustjose <stovallj1995@gmail.com>
2019-02-13 20:10:42 -0500

    Rewrite sample placement algorithms to support other dims

--- src/main/java/com/oitsjustjose/geolosys/common/world/ChunkData.java
+++ src/main/java/com/oitsjustjose/geolosys/common/world/ChunkData.java
@@ -3,11 +3,15 @@ package com.oitsjustjose.geolosys.common.world;
 import java.util.Random;
 
 import com.oitsjustjose.geolosys.common.api.GeolosysAPI;
+import com.oitsjustjose.geolosys.common.api.GeolosysAPI.ChunkPosSerializable;
 import com.oitsjustjose.geolosys.common.blocks.BlockSample;
 import com.oitsjustjose.geolosys.common.blocks.BlockSampleVanilla;
 import com.oitsjustjose.geolosys.common.config.ModConfig;
 
+import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
@@ -17,7 +21,7 @@ public class ChunkData
 {
     private Random random = new Random();
 
-    public void addChunk(ChunkPos pos, World world, IBlockState state)
+    public void addChunk(ChunkPos pos, World world, IBlockState state, int depositHeight)
     {
         if (world.getWorldType() == WorldType.FLAT)
         {
@@ -27,7 +31,7 @@ public class ChunkData
         int cap = getSampleCount(state);
         for (int i = 0; i < cap; i++)
         {
-            BlockPos p = getSamplePos(world, pos);
+            BlockPos p = getSamplePos(world, pos, depositHeight);
 
             if (world.getBlockState(p.down()).getBlock() instanceof BlockSample
                     || world.getBlockState(p.down()).getBlock() instanceof BlockSampleVanilla)
@@ -48,10 +52,65 @@ public class ChunkData
                 .contains(new GeolosysAPI.ChunkPosSerializable(pos, dimension));
     }
 
-    private BlockPos getSamplePos(World world, ChunkPos chunkPos)
+    /**
+     * A bottom-up search for the next "surface-like" block.
+     */
+    private BlockPos getSamplePos(World world, ChunkPos chunkPos, int depositHeight)
     {
-        return world.getTopSolidOrLiquidBlock(
-                new BlockPos((chunkPos.x << 4) + random.nextInt(16), 0, (chunkPos.z << 4) + random.nextInt(16)));
+        int blockPosX = (chunkPos.x << 4) + random.nextInt(16);
+        int blockPosZ = (chunkPos.z << 4) + random.nextInt(16);
+        BlockPos searchPos = new BlockPos(blockPosX, 0, blockPosZ);
+        while (searchPos.getY() < world.getHeight())
+        {
+            if (world.getBlockState(searchPos.down()).isSideSolid(world, searchPos.down(), EnumFacing.UP))
+            {
+                // If the current block is air
+                if (canReplace(world, searchPos))
+                {
+                    // If the block above this state is air,
+                    if (canReplace(world, searchPos.up()))
+                    {
+                        // If it's above sea level it's fine
+                        if (searchPos.getY() > world.getSeaLevel())
+                        {
+                            // Set the block above to air because we have a whitelist system
+                            world.setBlockState(searchPos.up(), Blocks.AIR.getDefaultState(), 2 | 16);
+                            return searchPos;
+                        }
+                        // If not, it's gotta be at least 12 blocks away from it (i.e. below it) but at least above the deposit
+                        else if (isWithinRange(world.getSeaLevel(), searchPos.getY(), 12)
+                                && searchPos.getY() < depositHeight)
+                        {
+                            // Set the block above to air because we have a whitelist system
+                            world.setBlockState(searchPos.up(), Blocks.AIR.getDefaultState(), 2 | 16);
+                            return searchPos;
+                        }
+                    }
+                }
+            }
+            searchPos = searchPos.up();
+        }
+        return world.getTopSolidOrLiquidBlock(searchPos);
+    }
+
+    private boolean canReplace(World world, BlockPos pos)
+    {
+        IBlockState state = world.getBlockState(pos);
+        Material mat = state.getMaterial();
+        return mat == Material.AIR || mat == Material.PLANTS || state.getBlock().isLeaves(state, world, pos)
+                || state.getBlock().isFoliage(world, pos) || mat.isReplaceable();
+    }
+
+    /**
+     * 
+     * @param posA
+     * @param posB
+     * @param range An integer representing how far is acceptable to be considered in range
+     * @return
+     */
+    private boolean isWithinRange(int posA, int posB, int range)
+    {
+        return (Math.abs(posA - posB) <= range);
     }
 
     private boolean isMoist(World world, BlockPos pos)
