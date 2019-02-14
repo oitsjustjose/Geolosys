8ff6372330acd32448dfed17845edce3d4f311cb
oitsjustjose <stovallj1995@gmail.com>
2019-02-14 15:10:00 -0500

    Fix TC compat not specifying Geolosys ore

--- src/main/java/com/oitsjustjose/geolosys/compat/ThaumcraftCompat.java
+++ src/main/java/com/oitsjustjose/geolosys/compat/ThaumcraftCompat.java
@@ -1,5 +1,8 @@
 package com.oitsjustjose.geolosys.compat;
 
+import com.oitsjustjose.geolosys.common.blocks.BlockOre;
+import com.oitsjustjose.geolosys.common.blocks.BlockOreVanilla;
+
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -10,6 +13,11 @@ public class ThaumcraftCompat
     @SubscribeEvent
     public void registerEvent(HarvestDropsEvent event)
     {
+        if (!(event.getState().getBlock() instanceof BlockOre
+                || event.getState().getBlock() instanceof BlockOreVanilla))
+        {
+            return;
+        }
         Random rand = new Random();
         if (rand.nextInt(10) == 0)
         {
