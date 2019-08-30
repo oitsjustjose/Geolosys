46f2d129ae03691901ca201d04bbaf1beee13d5d
oitsjustjose <stovallj1995@gmail.com>
2019-08-30 09:54:25 -0400

    Finish up utils, move to manual package

--- src/main/java/com/oitsjustjose/geolosys/common/manual/PatronUtil.java
+++ src/main/java/com/oitsjustjose/geolosys/common/manual/PatronUtil.java
@@ -1,10 +1,12 @@
-package com.oitsjustjose.geolosys.common.util;
+package com.oitsjustjose.geolosys.common.manual;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
+
+import com.oitsjustjose.geolosys.Geolosys;
 
 public class PatronUtil
 {
@@ -34,23 +36,33 @@ public class PatronUtil
     public void init()
     {
         this.patrons = new ArrayList<>();
+        Geolosys.getInstance().LOGGER.info("Fetching Patrons from the web...");
         try
         {
+
             URL url = new URL("http://patreon.forgeserv.net");
 
             BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
 
             String line;
+            StringBuilder allData = new StringBuilder();
             while ((line = in.readLine()) != null)
             {
-                System.out.println(line);
+                allData.append(line);
             }
-
             in.close();
-
+            for (String patronName : allData.toString().split("<br>"))
+            {
+                this.patrons.add(patronName);
+            }
         }
         catch (IOException e)
         {
+            Geolosys.getInstance().LOGGER.error("Patrons could not be fetched - are you connected to the internet?");
+        }
+        finally
+        {
+            Geolosys.getInstance().LOGGER.info("Patrons successfully fetched!");
         }
     }
 }
