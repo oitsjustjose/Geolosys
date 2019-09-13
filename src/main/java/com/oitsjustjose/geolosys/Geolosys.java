88cd73e1c02a75ec35d6f38bd85a1a7d8aa87da4
Jose Stovall <stovallj1995@gmail.com>
2019-09-12 21:07:49 -0400

    The true initial commit

--- src/main/java/com/oitsjustjose/geolosys/Geolosys.java
+++ src/main/java/com/oitsjustjose/geolosys/Geolosys.java
@@ -1,129 +1,91 @@
 package com.oitsjustjose.geolosys;
 
-import com.oitsjustjose.geolosys.client.ClientRegistry;
-import com.oitsjustjose.geolosys.common.CommonProxy;
-import com.oitsjustjose.geolosys.common.blocks.BlockOre;
-import com.oitsjustjose.geolosys.common.blocks.BlockOreVanilla;
-import com.oitsjustjose.geolosys.common.blocks.BlockSample;
-import com.oitsjustjose.geolosys.common.blocks.BlockSampleVanilla;
-import com.oitsjustjose.geolosys.common.config.ConfigOres;
-import com.oitsjustjose.geolosys.common.config.ConfigParser;
-import com.oitsjustjose.geolosys.common.config.ModConfig;
-import com.oitsjustjose.geolosys.common.items.ItemCluster;
-import com.oitsjustjose.geolosys.common.items.ItemCoal;
-import com.oitsjustjose.geolosys.common.items.ItemCoalCoke;
-import com.oitsjustjose.geolosys.common.items.ItemFieldManual;
-import com.oitsjustjose.geolosys.common.items.ItemIngot;
-import com.oitsjustjose.geolosys.common.items.ItemProPick;
-import com.oitsjustjose.geolosys.common.util.Recipes;
-import com.oitsjustjose.geolosys.common.world.ChunkData;
-import com.oitsjustjose.geolosys.common.world.OreGenerator;
-import com.oitsjustjose.geolosys.common.world.StoneGenerator;
-import com.oitsjustjose.geolosys.common.world.VanillaWorldGenOverride;
-import com.oitsjustjose.geolosys.compat.CompatLoader;
+import java.util.ArrayList;
 
+import com.oitsjustjose.geolosys.blocks.Types;
+
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.Block.Properties;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(modid = Geolosys.MODID, name = "Geolosys", version = Geolosys.VERSION, acceptedMinecraftVersions = "1.12", dependencies = "after:immersiveengineering@[0.12,);after:contenttweaker;")
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("geolosys")
 public class Geolosys
 {
-    public static final String MODID = "geolosys";
-    public static final String VERSION = "@VERSION@";
-    @SidedProxy(clientSide = "com.oitsjustjose.geolosys.client.ClientProxy", serverSide = "com.oitsjustjose.geolosys.common.CommonProxy")
-    public static CommonProxy proxy;
-    @Instance(Geolosys.MODID)
-    private static Geolosys instance;
-    // Logger & Configs, statically accessible.
-    public Logger LOGGER;
-    public ConfigOres configOres;
-    public ClientRegistry clientRegistry;
-    public ChunkData chunkOreGen;
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
 
-    public BlockOre ORE;
-    public BlockOreVanilla ORE_VANILLA;
-    public BlockSample ORE_SAMPLE;
-    public BlockSampleVanilla ORE_SAMPLE_VANILLA;
+    private static ArrayList<Block> blocks = new ArrayList<>();
 
-    public Item CLUSTER;
-    public Item INGOT;
-    public Item COAL;
-    public Item COAL_COKE;
-    public ItemProPick PRO_PICK;
-    public Item ALMANAC;
-
-    public static Geolosys getInstance()
+    public Geolosys()
     {
-        return instance;
+        for (Types.Vanilla vanillaType : Types.Vanilla.values())
+        {
+            Properties blockProp = Properties.create(Material.ROCK, MaterialColor.STONE)
+                    .hardnessAndResistance(7.5F, 10F).sound(SoundType.STONE).harvestLevel(vanillaType.getToolLevel())
+                    .harvestTool(ToolType.PICKAXE);
+            Block block = new Block(blockProp).setRegistryName("geolosys", "ore_" + vanillaType.getName());
+            blocks.add(block);
+        }
+
+        for (Types.Modded moddedType : Types.Modded.values())
+        {
+            Properties blockProp = Properties.create(Material.ROCK, MaterialColor.STONE)
+                    .hardnessAndResistance(7.5F, 10F).sound(SoundType.STONE).harvestLevel(moddedType.getToolLevel())
+                    .harvestTool(ToolType.PICKAXE);
+            Block block = new Block(blockProp).setRegistryName("geolosys", "ore_" + moddedType.getName());
+            blocks.add(block);
+        }
+        // Block testBlock = new Block(Properties.create(Material.WOOL, MaterialColor.WOOL)
+        // .hardnessAndResistance(0.5F, 0.0F).sound(SoundType.SLIME)).setRegistryName("geolosys", "test_block");
+        // blocks.add(testBlock);
+
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
+    private void setup(final FMLCommonSetupEvent event)
     {
-        LOGGER = event.getModLog();
-        clientRegistry = new ClientRegistry();
-        MinecraftForge.EVENT_BUS.register(clientRegistry);
-        MinecraftForge.EVENT_BUS.register(new ModConfig.EventHandler());
-        chunkOreGen = new ChunkData();
-
-        ORE = new BlockOre();
-        ORE_SAMPLE = new BlockSample();
-        ORE_VANILLA = new BlockOreVanilla();
-        ORE_SAMPLE_VANILLA = new BlockSampleVanilla();
-        CLUSTER = new ItemCluster();
-        ALMANAC = new ItemFieldManual();
-
-        if (ModConfig.featureControl.enableIngots)
-        {
-            INGOT = new ItemIngot();
-        }
-        if (ModConfig.featureControl.enableCoals)
-        {
-            COAL = new ItemCoal();
-        }
-        if (ModConfig.prospecting.enableProPick)
-        {
-            PRO_PICK = new ItemProPick();
-        }
-        if (Loader.isModLoaded("immersiveengineering") && ModConfig.compat.enableIECompat
-                && ModConfig.featureControl.enableCoals)
-        {
-            COAL_COKE = new ItemCoalCoke();
-        }
-        configOres = new ConfigOres(event.getModConfigurationDirectory());
     }
 
-    @EventHandler
-    public void init(FMLInitializationEvent event)
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents
     {
-        proxy.init(event);
-        MinecraftForge.ORE_GEN_BUS.register(new VanillaWorldGenOverride());
-        configOres.init();
-        ConfigParser.init();
-        CompatLoader.init();
-    }
-
-    @EventHandler
-    public void postInit(FMLPostInitializationEvent event)
-    {
-        configOres.postInit();
-        if (ModConfig.featureControl.enableSmelting)
+        @SubscribeEvent
+        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent)
         {
-            Recipes.init(configOres, CLUSTER);
+            for (Block b : blocks)
+            {
+                blockRegistryEvent.getRegistry().register(b);
+            }
         }
-        proxy.postInit(event);
-        GameRegistry.registerWorldGenerator(new OreGenerator(), 0);
-        GameRegistry.registerWorldGenerator(new StoneGenerator(), 100);
+
+        @SubscribeEvent
+        public static void onItemsRegistry(final RegistryEvent.Register<Item> itemRegistryEvent)
+        {
+            for (Block b : blocks)
+            {
+                Item iBlock = new BlockItem(b, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS))
+                        .setRegistryName(b.getRegistryName());
+                itemRegistryEvent.getRegistry().register(iBlock);
+            }
+        }
     }
 }
---
+++ src/main/resources/assets/geolosys/generator.py
@@ -0,0 +1,115 @@
+"""
+    Author: Jose Stovall
+    Geolosys: https://github.com/oitsjustjose/Geolosys
+"""
+import json
+import sys
+
+from pip._vendor.colorama import Fore
+
+
+def generate_states(states: list) -> None:
+    for state in states:
+        json_doc = {"variants": {"": [{"model": "geolosys:block/{}".format(state)}]}}
+        with open("./blockstates/{}.json".format(state), "w") as file:
+            file.write(json.dumps(json_doc))
+            print(
+                Fore.BLUE
+                + "Wrote ./blockstates/{}.json to disk".format(state)
+                + Fore.RESET
+            )
+
+
+def generate_item_models(states: list) -> None:
+    for state in states:
+        json_doc = {
+            "parent": "block/cube_all",
+            "textures": {"all": "geolosys:blocks/{}".format(state)},
+        }
+        with open("./models/block/{}.json".format(state), "w") as file:
+            file.write(json.dumps(json_doc))
+            print(Fore.BLUE + "Wrote ./models/block/{}.json to disk".format(state) + Fore.RESET)
+
+
+def generate_block_models(states: list) -> None:
+    for state in states:
+        json_doc = {"parent": "geolosys:block/{}".format(state)}
+        with open("./models/item/{}.json".format(state), "w") as file:
+            file.write(json.dumps(json_doc))
+            print(Fore.BLUE + "Wrote ./models/item/{}.json to disk".format(state) + Fore.RESET)
+
+
+def main(block_state: bool, item_model: bool, block_model: bool) -> None:
+    """
+    The main program logic which generates blockstates based on the array below
+    Args:
+        block_state (bool): trigger whether or not to generate blockstates
+        item_model (bool): trigger whether or not to generate item models
+        block_model (bool): trigger whether or not to generate block models
+    """
+    states = [
+        "ore_hematite",
+        "ore_limonite",
+        "ore_malachite",
+        "ore_azurite",
+        "ore_cassiterite",
+        "ore_teallite",
+        "ore_galena",
+        "ore_bauxite",
+        "ore_platinum",
+        "ore_autunite",
+        "ore_sphalerite",
+        "ore_coal",
+        "ore_cinnabar",
+        "ore_gold",
+        "ore_lapis",
+        "ore_quartz",
+        "ore_kimberlite",
+        "ore_beryl",
+    ]
+
+    if block_state:
+        generate_states(states)
+    if item_model:
+        generate_item_models(states)
+    if block_model:
+        generate_block_models(states)
+
+
+if __name__ == "__main__":
+
+    def print_help() -> None:
+        """Prints the help prompt for the user to know their options"""
+        print(Fore.YELLOW + "Possible arguments:" + Fore.RESET)
+        print(Fore.CYAN + "    -i: Generate Item Models" + Fore.RESET)
+        print(Fore.CYAN + "    -b: Generate Block Models" + Fore.RESET)
+        print(Fore.CYAN + "    -s: Generate BlockStates" + Fore.RESET)
+        print(Fore.CYAN + "    -a: Generate All" + Fore.RESET)
+        print(Fore.CYAN + "    -h, ? : This screen" + Fore.RESET)
+
+    possible_args = {
+        "-a": False,
+        "-i": False,
+        "-b": False,
+        "-s": False,
+        "-h": False,
+        "?": False,
+    }
+
+    for arg in sys.argv[1:]:
+        for arg_name in possible_args:
+            if arg_name in arg:
+                possible_args[arg_name] = True
+    if not any(list(possible_args.values())):
+        print(Fore.RED + "No arguments given." + Fore.RESET)
+        print_help()
+        exit()
+
+    if possible_args["-h"] or possible_args["?"]:
+        print_help()
+        exit()
+
+    if possible_args["-a"]:
+        main(True, True, True)
+    else:
+        main(possible_args["-s"], possible_args["-i"], possible_args["-b"])
