1809ff1bc86048a72401adb667ec7146bfcffd2a
oitsjustjose <stovallj1995@gmail.com>
2019-09-18 09:41:01 -0400

    Init worldgen overrides

---
+++ src/main/java/com/oitsjustjose/geolosys/common/world/WorldGenOverrides.java
@@ -0,0 +1,14 @@
+package com.oitsjustjose.geolosys.common.world;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.gen.feature.OreFeature;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class WorldGenOverrides
+{
+    public static void init()
+    {
+        OreFeature ore = (OreFeature) ForgeRegistries.FEATURES.getValue(new ResourceLocation("minecraft", "ore"));
+        ForgeRegistries.FEATURES.getValues().remove(ore);
+    }
+}
