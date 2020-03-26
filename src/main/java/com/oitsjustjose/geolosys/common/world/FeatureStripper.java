84f8f90ab62a4733784fbd16bc21df55294dfff7
oitsjustjose <stovallj1995@gmail.com>
2020-03-26 13:51:28 -0400

    Fix CME's when removing vanilla ores (closes #239)

--- src/main/java/com/oitsjustjose/geolosys/common/world/FeatureStripper.java
+++ src/main/java/com/oitsjustjose/geolosys/common/world/FeatureStripper.java
@@ -1,8 +1,5 @@
 package com.oitsjustjose.geolosys.common.world;
 
-import java.util.ConcurrentModificationException;
-
-import com.oitsjustjose.geolosys.Geolosys;
 import com.oitsjustjose.geolosys.common.utils.Utils;
 
 import net.minecraft.block.BlockState;
@@ -23,78 +20,63 @@ public class FeatureStripper
             Blocks.LAPIS_ORE.getDefaultState(), Blocks.NETHER_QUARTZ_ORE.getDefaultState(),
             Blocks.REDSTONE_ORE.getDefaultState() };
 
-    public static void strip(int numTries)
+    public static void strip()
     {
-        if (numTries == 3)
+        for (Biome biome : ForgeRegistries.BIOMES.getValues())
         {
-            Geolosys.getInstance().LOGGER.error(
-                    "Geolosys continued to received concurrent modification exceptions through all 3 additional attempts");
-            return;
-        }
-        try
-        {
-            for (Biome biome : ForgeRegistries.BIOMES.getValues())
-            {
-                biome.getFeatures(GenerationStage.Decoration.UNDERGROUND_ORES).removeIf((x) -> {
-                    if (x.config instanceof DecoratedFeatureConfig)
+            biome.getFeatures(GenerationStage.Decoration.UNDERGROUND_ORES).removeIf((x) -> {
+                if (x.config instanceof DecoratedFeatureConfig)
+                {
+                    DecoratedFeatureConfig decConf = (DecoratedFeatureConfig) x.config;
+                    // Handles most ores
+                    if (decConf.feature.config instanceof OreFeatureConfig)
                     {
-                        DecoratedFeatureConfig decConf = (DecoratedFeatureConfig) x.config;
-                        // Handles most ores
-                        if (decConf.feature.config instanceof OreFeatureConfig)
-                        {
-                            OreFeatureConfig featureConf = (OreFeatureConfig) decConf.feature.config;
+                        OreFeatureConfig featureConf = (OreFeatureConfig) decConf.feature.config;
 
-                            for (BlockState state2 : match)
-                            {
-                                if (Utils.doStatesMatch(featureConf.state, state2))
-                                {
-                                    return true;
-                                }
-                            }
-                        }
-                        // Handles extra emeralds in mountains
-                        else if (decConf.feature.config instanceof ReplaceBlockConfig)
+                        for (BlockState state2 : match)
                         {
-                            ReplaceBlockConfig featureConf = (ReplaceBlockConfig) decConf.feature.config;
-
-                            for (BlockState state2 : match)
+                            if (Utils.doStatesMatch(featureConf.state, state2))
                             {
-                                if (Utils.doStatesMatch(featureConf.state, state2))
-                                {
-                                    return true;
-                                }
+                                return true;
                             }
                         }
                     }
-                    return false;
-                });
-                // Handles Nether Ores
-                biome.getFeatures(GenerationStage.Decoration.UNDERGROUND_DECORATION).removeIf((x) -> {
-                    if (x.config instanceof DecoratedFeatureConfig)
+                    // Handles extra emeralds in mountains
+                    else if (decConf.feature.config instanceof ReplaceBlockConfig)
                     {
-                        DecoratedFeatureConfig decConf = (DecoratedFeatureConfig) x.config;
-                        if (decConf.feature.config instanceof OreFeatureConfig)
-                        {
-                            OreFeatureConfig featureConf = (OreFeatureConfig) decConf.feature.config;
+                        ReplaceBlockConfig featureConf = (ReplaceBlockConfig) decConf.feature.config;
 
-                            for (BlockState state2 : match)
+                        for (BlockState state2 : match)
+                        {
+                            if (Utils.doStatesMatch(featureConf.state, state2))
                             {
-                                if (Utils.doStatesMatch(featureConf.state, state2))
-                                {
-                                    return true;
-                                }
+                                return true;
                             }
                         }
                     }
-                    return false;
-                });
-            }
-        }
-        catch (ConcurrentModificationException cme)
-        {
-            Geolosys.getInstance().LOGGER
-                    .warn("Removing ores from biomes caused a concurrent modification. Will try again 3 more times...");
-            strip(numTries + 1);
+                }
+                return false;
+            });
+            // Handles Nether Ores
+            biome.getFeatures(GenerationStage.Decoration.UNDERGROUND_DECORATION).removeIf((x) -> {
+                if (x.config instanceof DecoratedFeatureConfig)
+                {
+                    DecoratedFeatureConfig decConf = (DecoratedFeatureConfig) x.config;
+                    if (decConf.feature.config instanceof OreFeatureConfig)
+                    {
+                        OreFeatureConfig featureConf = (OreFeatureConfig) decConf.feature.config;
+
+                        for (BlockState state2 : match)
+                        {
+                            if (Utils.doStatesMatch(featureConf.state, state2))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+                return false;
+            });
         }
     }
 }
