23bcf7b1322c08d132c9f0155c416208b4916b15
oitsjustjose <stovallj1995@gmail.com>
2021-06-07 09:00:49 -0400

    Remove unused method

--- src/main/java/com/oitsjustjose/geolosys/common/world/OreRemover.java
+++ src/main/java/com/oitsjustjose/geolosys/common/world/OreRemover.java
@@ -1,8 +1,6 @@
 package com.oitsjustjose.geolosys.common.world;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Supplier;
@@ -13,16 +11,12 @@ import com.oitsjustjose.geolosys.common.world.feature.FeatureUtils;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.world.gen.GenerationStage.Decoration;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.feature.DecoratedFeatureConfig;
-import net.minecraft.world.gen.feature.IFeatureConfig;
 import net.minecraft.world.gen.feature.NoExposedOreFeature;
 import net.minecraft.world.gen.feature.OreFeature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
 import net.minecraft.world.gen.feature.ReplaceBlockConfig;
 import net.minecraft.world.gen.feature.ReplaceBlockFeature;
-import net.minecraftforge.common.world.BiomeGenerationSettingsBuilder;
 
 public class OreRemover {
 
@@ -68,48 +62,5 @@ public class OreRemover {
 
         }
         return removed;
-    }
-
-    // Not used anymore, you may remove
-    public static int process(BiomeGenerationSettingsBuilder settings) {
-        Collection<Supplier<ConfiguredFeature<?, ?>>> toKeep = new ArrayList<Supplier<ConfiguredFeature<?, ?>>>();
-        int total = 0;
-        int newTotal = 0;
-
-        for (Decoration stage : Decoration.values()) {
-            int start = settings.getFeatures(stage).size();
-            total += start;
-
-            settings.getFeatures(stage).forEach((supp) -> {
-                ConfiguredFeature<?, ?> configuredFeature = supp.get();
-                IFeatureConfig config = configuredFeature.config;
-
-                while (config instanceof DecoratedFeatureConfig) {
-                    DecoratedFeatureConfig dconfig = (DecoratedFeatureConfig) config;
-                    config = dconfig.feature.get().config;
-                }
-
-                if (config instanceof OreFeatureConfig) {
-                    OreFeatureConfig conf = (OreFeatureConfig) config;
-                    if (!toRm.contains(conf.state.getBlock())) {
-                        toKeep.add(supp);
-                    }
-                } else if (config instanceof ReplaceBlockConfig) {
-                    ReplaceBlockConfig conf = (ReplaceBlockConfig) config;
-                    if (!toRm.contains(conf.state.getBlock())) {
-                        toKeep.add(supp);
-                    }
-                } else {
-                    toKeep.add(supp);
-                }
-            });
-
-            settings.getFeatures(stage).clear();
-            settings.getFeatures(stage).addAll(toKeep);
-
-            newTotal += settings.getFeatures(stage).size();
-        }
-
-        return newTotal - total;
     }
 }
