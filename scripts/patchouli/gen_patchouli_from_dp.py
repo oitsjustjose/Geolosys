cf12499241d24ad43d0750d666fd5fa45fc011c0
oitsjustjose <stovallj1995@gmail.com>
2021-12-13 08:53:21 -0600

    Update patchouli docs, gen script

--- scripts/patchouli/gen_patchouli_from_dp.py
+++ scripts/patchouli/gen_patchouli_from_dp.py
@@ -3,20 +3,23 @@ import sys
 import json
 from typing import List
 
+
 def to_proper(s: str) -> str:
     ret = []
     for x in s.split(" "):
         ret.append(x[0].upper() + x[1:])
     return " ".join(ret)
 
+
 def strip_namespace(s: str) -> str:
     if ":" not in s:
         return s
-    return s[s.index(":")+1:]
+    return s[s.index(":") + 1 :]
+
 
 def fancy_join(l: List[str], use_or=False) -> str:
     size = len(l) - 1
-    
+
     if len(l) == 0:
         return l[0]
 
@@ -30,26 +33,30 @@ def fancy_join(l: List[str], use_or=False) -> str:
                 ret += ", "
     return ret
 
+
 def get_name(config: dict) -> str:
-    blocks = list(map(lambda x: x["block"], config["blocks"]))
+    blocks = list(map(lambda x: x["block"], config["blocks"]["default"]))
     blocks = list(filter(lambda x: x is not None, blocks))
     blocks = list(map(lambda x: x.replace("_", " "), blocks))
     blocks = list(map(strip_namespace, blocks))
     blocks = list(map(to_proper, blocks))
     return fancy_join(blocks)
 
+
 def generate_dim_spec(config: dict) -> str:
     if "dimensions" in config:
-        if config['dimensions']['isBlacklist'] == True:
+        if config["dimensions"]["isBlacklist"] == True:
             return f"cannot be found in the {fancy_join(config['dimensions']['filter'], use_or=True).replace('the_', '')}"
         return f"can only be found in the {fancy_join(config['dimensions']['filter'], use_or=True).replace('the_', '')}"
     return f"cannot be found in the {fancy_join(config['dimBlacklist']).replace('the_', '')}"
+
 
 def sign_depth(val: int) -> str:
     diff = 64 - val
     if diff > 0:
         return f"{abs(diff)} meters below"
     return f"{abs(diff)} meters above"
+
 
 def get_biome_info(config: dict) -> str:
     if "biomes" in config:
@@ -61,9 +68,10 @@ def get_biome_info(config: dict) -> str:
         return f"is only found in {biomes} biomes"
     return "can be found in any biome"
 
+
 def get_depth_info(data) -> str:
     try:
-        if data['type'] == "geolosys:deposit_top_layer":
+        if data["type"] == "geolosys:deposit_top_layer":
             samples = list(map(lambda x: x["block"], data["config"]["samples"]))
             samples = list(filter(lambda x: x is not None, samples))
             samples = list(map(lambda x: x.replace("_", " "), samples))
@@ -80,7 +88,9 @@ def get_depth_info(data) -> str:
 def gen(data: dict) -> dict:
     type: str = to_proper(data["type"].replace("geolosys:deposit_", ""))
     config: dict = data["config"]
-    item = config["blocks"][0]["block"] if "blocks" in config else config["block"]
+    # Deprecated
+    # item = config["blocks"][0]["block"] if "blocks" in config else config["block"]
+    item = config["blocks"]["default"][0]["block"]
     name = get_name(config).replace(" Ore", "")
     ret = {
         "name": name,
@@ -92,28 +102,27 @@ def gen(data: dict) -> dict:
                 "title": name,
                 "item": item,
                 "link_recipe": False,
-                "text": f"This {' '.join(type.lower().split('_'))} deposit {get_depth_info(data)}. It {generate_dim_spec(config)} and {get_biome_info(config)}."
+                "text": f"This {' '.join(type.lower().split('_'))} deposit {get_depth_info(data)}. It {generate_dim_spec(config)} and {get_biome_info(config)}.",
             }
-        ]
+        ],
     }
 
     return ret
+
 
 if len(sys.argv) != 2:
     print("Usage: gen_patchouli_from_dp <DEPOSITS_DIR>")
     sys.exit(0)
 
 for root, _, files in os.walk(sys.argv[1]):
-    if not os.path.exists('./out'):
-        os.mkdir('./out')
+    if not os.path.exists("./out"):
+        os.mkdir("./out")
     for file in files:
         with open(f"{root}/{file}", "r") as fp:
             data = json.loads(fp.read())
-        
+
         config = data
         output_d = gen(config)
 
         with open(f"./out/{file}", "w") as fp:
             fp.write(json.dumps(output_d))
-
-            
